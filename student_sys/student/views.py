6a083dc85b376cadab6cec5ffc704f410b654e4c
Chauncey2 <gaochangju95@163.com>
2021-09-06 02:36:19 +0800

    CBV

--- student_sys/student/views.py
+++ student_sys/student/views.py
@@ -1,26 +1,57 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.views import View
 from .models import Student
 from .forms import StudentForm
 
 
 # Create your views here.
-def index(request):
-    # student = Student.objects.all()
-    student = Student.get_all()
-    if request.method == "POST":
+# def index(request):
+#     # student = Student.objects.all()
+#     student = Student.get_all()
+#     if request.method == "POST":
+#         form = StudentForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             print(reverse('index'))
+#             return HttpResponseRedirect(reverse('index'))
+#     else:
+#         form = StudentForm()
+#
+#     context = {
+#         'students': student,
+#         'form': form
+#     }
+#
+#     return render(request, 'index.html', context=context)
+
+
+class IndexView(View):
+    template_name = 'index.html'
+
+    def get_context(self):
+        students = Student.get_all()
+        context = {
+            'students': students
+        }
+        return context
+
+    def get(self, request):
+        context = self.get_context()
+        form = StudentForm()
+        context.update({
+            'form': form
+        })
+        return render(request, self.template_name, context=context)
+
+    def post(self, request):
         form = StudentForm(request.POST)
         if form.is_valid():
             form.save()
-            print(reverse('index'))
             return HttpResponseRedirect(reverse('index'))
-    else:
-        form = StudentForm()
-
-    context = {
-        'students': student,
-        'form': form
-    }
-
-    return render(request, 'index.html', context=context)
+        context = self.get_context()
+        context.update({
+            'form': form
+        })
+        return render(request, self.template_name, context=context)
