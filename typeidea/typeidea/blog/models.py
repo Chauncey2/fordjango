778d3f1e2ddfe775061322e1bf912256752a3b1f
Chauncey2 <gaochangju95@163.com>
2021-09-11 23:07:57 +0800

    update blog model

--- typeidea/typeidea/blog/models.py
+++ typeidea/typeidea/blog/models.py
@@ -23,7 +23,19 @@ class Category(models.Model):
 
 # 标签
 class Tag(models.Model):
-    pass
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除')
+    )
+    name = models.CharField(max_length=128, verbose_name="名称")
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="作者")
+    ceeate_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = verbose_name_plural = "文章"
 
 
 # 提交的文章
