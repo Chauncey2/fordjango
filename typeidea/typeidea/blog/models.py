b7bb0d5753492eac443e69d50e96117fa40d8884
Chauncey2 <gaochangju95@163.com>
2021-09-12 22:43:52 +0800

    add configapp

--- typeidea/typeidea/blog/models.py
+++ typeidea/typeidea/blog/models.py
@@ -32,7 +32,7 @@ class Tag(models.Model):
     name = models.CharField(max_length=128, verbose_name="名称")
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="作者")
-    ceeate_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
         verbose_name = verbose_name_plural = "文章"
@@ -40,4 +40,22 @@ class Tag(models.Model):
 
 # 提交的文章
 class Post(models.Model):
-    pass
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_DRAFT = 2
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除'),
+        (STATUS_DRAFT, '草稿')
+    )
+    title = models.CharField(max_length=128, verbose_name="标题")
+    desc = models.CharField(max_length=1024, verbose_name="摘要")
+    content = models.TextField(verbose_name="正文", help_text="正文必须使用Markdown格式")
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="分类")
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name="标签")
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = verbose_name_plural = "文章"
+        ordering = ['-id']  # 根据id进行降序排序
